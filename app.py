08b725235cdc2174d3645bf32dd949c201623601
Adriana Garcia <adri@Adrianas-MacBook-Air.local>
2022-06-20 23:34:03 -0700

    Challenge

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import Scraping
 
 app = Flask(__name__)
 
@@ -13,12 +13,12 @@ def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-   @app.route("/scrape")
+@app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+    mars = mongo.db.mars
+    mars_data = Scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
 
-   if __name__ == "__main__":
-   app.run()
+if __name__ == "__main__":
+    app.run()
